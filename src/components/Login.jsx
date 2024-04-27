b15426a5bf1c780d4fe8a75ef19131d37a3c30a8
Prajwol Karki <prajwolkarki609@gmail.com>
2024-04-27 16:41:18 +0545

    login section completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,70 +1,97 @@
-import {Link} from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { Link, useNavigate } from "react-router-dom";
 const Login = () => {
-  let [userCred,setUserCred] = useState({
-    username:"",
-    password:""
-  })
-  let [message,setMessage] = useState({
-    type:"invisible-msg",
-    text:"random-msg"
-  })
-  function handleInput(e){
-    
-    setUserCred((prev)=>{
-      return{
+  const loggedInData = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const [userCred, setUserCred] = useState({
+    username: "",
+    password: "",
+  });
+  const [message, setMessage] = useState({
+    type: "invisible-msg",
+    text: "random-msg",
+  });
+
+  function handleInput(e) {
+    setUserCred((prev) => {
+      return {
         ...prev,
-        [e.target.name]:e.target.value
-      }
-    })
+        [e.target.name]: e.target.value,
+      };
+    });
   }
   function handleSubmit(e) {
     e.preventDefault();
-    fetch('http://localhost:5000/login', {
+    fetch("http://localhost:5000/login", {
       method: "POST",
       body: JSON.stringify(userCred),
-      headers: { // 'headers', not 'header'
-        'Content-Type': 'application/json'
-      }
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
       .then((res) => {
-       // console.log(res);
-        if(res.status === 404){
+        // console.log(res);
+        if (res.status === 404) {
           setMessage({
-            type:"error",
-            text:"User not found"
-          })
-        }else if(res.status === 403){
+            type: "error",
+            text: "User not found",
+          });
+        } else if (res.status === 403) {
           setMessage({
-            type:"error",
-            text:"Incorrect Password"
-          })
+            type: "error",
+            text: "Incorrect Password",
+          });
         }
-        setTimeout(()=>{
-          setMessage({type:"invisible-msg",text:"Dummy Msg"})
-      },5000)
+        setTimeout(() => {
+          setMessage({ type: "invisible-msg", text: "Dummy Msg" });
+        }, 3000);
 
-      return res.json();
+        return res.json();
       })
       .then((data) => {
-        console.log(data);
+        //console.log(data);
+        if (data.token !== undefined) {
+          localStorage.setItem("userData", JSON.stringify(data));
+          loggedInData.setLoggedIn(data);
+          navigate("/track");
+        }
       })
       .catch((err) => {
         console.log(err);
       });
   }
-  
+
   return (
     <section className="container">
-        <form onSubmit={handleSubmit} className="form">
-            <h1>Sign In</h1>
-            <input type="text" name="username" className="input" placeholder="Enter your username" onChange={handleInput}/>
-            <input type="password" name="password" className="input" placeholder="Enter your password" onChange={handleInput}/>
-            <button className="btn">Log In</button>
-            <p className='below'>Not Registered? <Link className="signIn" to="/register">Sign up now</Link></p>
-        </form>
+      <form onSubmit={handleSubmit} className="form">
+        <h1>Sign In</h1>
+        <input
+          type="text"
+          name="username"
+          className="input"
+          placeholder="Enter your username"
+          onChange={handleInput}
+        />
+        <input
+          type="password"
+          name="password"
+          className="input"
+          placeholder="Enter your password"
+          onChange={handleInput}
+        />
+        <button className="btn">Log In</button>
+        <p className="below">
+          Not Registered?{" "}
+          <Link className="signIn" to="/register">
+            Sign up now
+          </Link>
+        </p>
+        <p className={message.type}>{message.text}</p>
+      </form>
     </section>
-  )
-}
+  );
+};
 
 export default Login;
